a1abc5e814d00dc446b84647bbb81de2c7207b1a
unknown <blechtova.tereza@gmail.com>
2020-03-26 20:42:17 -0700

    Renaming entry-point to home

--- routes/entry-point.js
+++
@@ -1,9 +0,0 @@
-
-
-const entry_point = (req, res) => {
-res.render('entry-point')
-
-//res.send('Hello from entry point module!');
-}
-module.exports = entry_point;
-
---
+++ routes/home.js
@@ -0,0 +1,9 @@
+
+
+const home = (req, res) => {
+res.render('home')
+
+//res.send('Hello from entry point module!');
+}
+module.exports = home;
+
