722ce751d45069fbd3e0c028557c9f52fc17d19e
Your Name <you@example.com>
2020-03-29 19:50:02 -0700

    Making home dipslay an array of objects

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,31 @@
 
 
 const home = (req, res) => {
-res.render('home')
+    const products = [{
+        id: 1,
+        name: "Fender",
+        description: "DR-100 Acoustic - Ebony",
+        price: 2599.99,
+        rating: 5,
+        image: "https://www.long-mcquade.com/files/1127/md_P_DR100-EB.jpg"
+    },
+    {
+        id: 2,
+        name: "Yamaha",
+        description: "DR-100 Acoustic - Red",
+        price: 2799.99,
+        rating: 5,
+        image: "https://www.long-mcquade.com/files/1127/md_P_DR100-EB.jpg"
+    },
+    {
+        id: 3,
+        name: "Gibson",
+        description: "DR-100 Acoustic - Black",
+        price: 2699.99,
+        rating: 5,
+        image: "https://www.long-mcquade.com/files/1127/md_P_DR100-EB.jpg"
+    }]
+res.render('home', {products})
 
 //res.send('Hello from entry point module!');
 }
